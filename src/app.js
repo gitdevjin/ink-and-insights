952255a024cb9de47a8f78bd31142a1787ac6732
gitdevjin <dev.hjshin@gmail.com>
2025-01-18 07:05:09 -0500

    pino-http added

--- src/app.js
+++ src/app.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 const logger = require('./logger');
 
+const pino = require('pino-http')({
+    logger,
+})
+
+app.use(pino);
+
 app.use('/', (req, res) => {
+    logger.info("Home");
     res.status(200).json("Hello World");
 })
 app.use('/hello', (req, res) => {
