04cef608b25ee4306405e9be4f1a3d987c0a1151
gitdevjin <dev.hjshin@gmail.com>
2025-02-25 22:18:07 -0500

    dockerize started

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const authenticate = require('../auth/authenticate');
+const multer = require('multer');
+
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.use('/auth', require('./auth'));
 
 router.post('/auth/test', authenticate, (req, res) => {
   res.status(200).json({ message: 'You are Authenticated' });
+});
+
+router.post('/post/test', upload.array('images'), (req, res) => {
+  console.log('**REQUEST BODY;');
+  console.log(req.body);
+  console.log(req.files);
+
+  console.log('**REQUEST BODY;');
+  res.status(200).json({ message: 'Recieved' });
 });
 
 router.get('/', (req, res) => {
