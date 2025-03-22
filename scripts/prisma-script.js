de332d330deea6c834d8bd9d61ab9e8795274891
gitdevjin <dev.hjshin@gmail.com>
2025-03-22 11:25:25 -0400

    category and subcategory table updated

--- scripts/prisma-script.js
+++ scripts/prisma-script.js
@@ -3,8 +3,45 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.deleteMany();
-  console.log(user);
+  // Find the 'Review' category
+  const reviewCategory = await prisma.category.findFirst({
+    where: { name: 'Review' },
+  });
+
+  if (reviewCategory) {
+    // Create subcategories for 'Review' category
+    await prisma.subCategory.createMany({
+      data: [
+        { name: 'Book Review', categoryId: reviewCategory.id },
+        { name: 'Movie Review', categoryId: reviewCategory.id },
+        { name: 'Anime Review', categoryId: reviewCategory.id },
+        { name: 'TV Show Review', categoryId: reviewCategory.id },
+      ],
+    });
+  } else {
+    console.log('Review category does not exist.');
+  }
+
+  // Find the 'Discussion' category
+  const discussionCategory = await prisma.category.findFirst({
+    where: { name: 'Discussion' },
+  });
+
+  if (discussionCategory) {
+    // Create subcategories for 'Discussion' category
+    await prisma.subCategory.createMany({
+      data: [
+        { name: 'Book Discussion', categoryId: discussionCategory.id },
+        { name: 'Movie Discussion', categoryId: discussionCategory.id },
+        { name: 'Anime Discussion', categoryId: discussionCategory.id },
+        { name: 'TV Show Discussion', categoryId: discussionCategory.id },
+      ],
+    });
+  } else {
+    console.log('Discussion category does not exist.');
+  }
+
+  console.log('Subcategories created successfully!');
 }
 
 main()
